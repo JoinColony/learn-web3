f614a71854b51f4fa8077c11c9a7e9e3a0ad7337
chmanie <code@chmanie.com>
2023-05-06 04:51:42 +0200

    Add simple block ingestor

---
+++ colony-missions/src/ingestor/index.ts
@@ -0,0 +1,40 @@
+import { ColonyEventManager } from '@colony/sdk'
+import { OneTxPaymentEvents__factory as OneTxPaymentEvents } from '@colony/events'
+
+import { provider } from '../colony'
+import { prisma } from '../prisma'
+
+const eventManager = new ColonyEventManager(provider)
+const oneTxEventSource = eventManager.createEventSource(OneTxPaymentEvents)
+
+setInterval(async () => {
+  const mission = await prisma.mission.findFirst({
+    where: {
+      NOT: [{txHash: null }],
+      paid: false,
+    }
+  })
+  if (!mission || !mission.txHash) {
+    return
+  }
+  console.log(`Found a pending transaction: ${mission.txHash}! Handling it...`)
+  const receipt = await provider.getTransactionReceipt(mission.txHash)
+  let foundEvent = null;
+  receipt.logs.forEach((log) => {
+    try {
+      foundEvent = oneTxEventSource.interface.parseLog(log)
+    } catch (e) {
+      // nothing
+    }
+  })
+  if (foundEvent) {
+    await prisma.mission.update({
+      where: {
+        id: mission.id
+      },
+      data: {
+        paid: true
+      }
+    })
+  }
+}, 5000)
